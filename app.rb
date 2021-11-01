6415e2efd1b6a900e210ddea5272a7a081254dd7
88Andybird <anbird88@gmail.com>
2021-11-01 16:54:46 +0000

    1st commit - Battle

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+
+get '/' do
+    "Hello World"
+  end
+
+  get '/secret' do
+      "This is a secret message"
+  end
+
+  get '/cat' do
+    erb(:index)
+
+  end
+  
