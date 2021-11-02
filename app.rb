58207c24c8315af4e70812b9d91fa251aecfa30e
88Andybird <anbird88@gmail.com>
2021-11-02 12:26:54 +0000

    Changed form from using get emthod to post method to hide querys in URL

--- app.rb
+++ app.rb
@@ -16,9 +16,13 @@ get '/' do
 
   end
 
-  get '/named-cat' do
+  post '/named-cat' do
     p params
     @name = params[:name]
     erb(:index)
+  end
+
+  get '/cat-form' do
+    erb :cat_form
   end
   
